aeb1d274a903750aa3100810d417372efd6ae412
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-02-15 20:28:18 +0200

    First commit. Initializing repository

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Manolescu Mihai Alexandru on 15/02/2018.
+//  Copyright © 2018 Manolescu Mihai Alexandru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
